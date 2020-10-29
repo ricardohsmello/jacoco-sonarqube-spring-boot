c20e68ad0ea06260862df3fc05959cc386122b12
Ricardo Mello <ricardo.mello@luizalabs.com>
2020-10-28 22:59:21 -0300

    first commit

---
+++ src/main/java/br/com/ricas/JacocoSonarqubeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoSonarqubeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoSonarqubeSpringBootApplication.class, args);
+	}
+
+}
