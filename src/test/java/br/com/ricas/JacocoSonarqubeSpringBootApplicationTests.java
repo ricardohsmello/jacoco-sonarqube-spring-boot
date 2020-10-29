5ad0ba2acbd3b6c09233484b8d54bfc5b8608ae2
Ricardo Mello <ricardo.mello@luizalabs.com>
2020-10-28 23:09:55 -0300

    including simple test

--- src/test/java/br/com/ricas/JacocoSonarqubeSpringBootApplicationTests.java
+++ src/test/java/br/com/ricas/JacocoSonarqubeSpringBootApplicationTests.java
@@ -3,11 +3,17 @@ package br.com.ricas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class JacocoSonarqubeSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try {
+			JacocoSonarqubeSpringBootApplication.main(new String[] {});
+		} catch (Exception e) {
+			fail(e);
+		}
 	}
-
 }
